cea453427906914f4691c9aea12c5b6bff5d9456
Stephen Simpson <me@simpo.org>
2012-12-18 11:46:11 +0000

    Updated the console so that error objects are passed correctly and do not fail.

--- worker/console.js
+++ worker/console.js
@@ -19,13 +19,66 @@ var console = {
 		}
 	},
 	error: function(){
+		var args = arguments;
+		if(arguments.length > 0){
+			args = new Array();
+			for(var i = 0; i < arguments.length; i++){
+				if(this._isErrorObject(arguments[i])){
+					args.push(this._convertErrorObjectToString(arguments[i]));
+				}else{
+					args.push(arguments[i]);
+				}
+			}
+		}
+		
 		console._global.postMessage({
-			"type": "error", "message": arguments
+			"type": "error", "message": args
 		});
 		if(orginalConsole){
 			if(orginalConsole.log){
 				orginalConsole.log(arguments);
 			}
+		}
+	},
+	_convertErrorObjectToString: function(obj){
+		if(obj.hasOwnProperty("response")){
+			obj.message = obj.response
+		}
+		if(obj.hasOwnProperty("code")){
+			obj.number = obj.code
+		}
+		
+		var errMessage = "Error"
+		if(obj.hasOwnProperty("number") || obj.hasOwnProperty("lineNumber")){
+			errMessage += ": ("
+			if(obj.hasOwnProperty("number")){
+				errMessage += obj.number.toString();
+			}
+			if(obj.hasOwnProperty("number") && obj.hasOwnProperty("lineNumber")){
+				errMessage += " - "
+			}
+			if(obj.hasOwnProperty("lineNumber")){
+				errMessage += "LineNo: " + obj.lineNumber.toString();
+			}
+			errMessage += ") "
+		}else{
+			errMessage += ": "
+		}
+		
+		if(obj.hasOwnProperty("message")){
+			errMessage += obj.message
+		}
+		if(obj.hasOwnProperty("fileName")){
+			errMessage += " (" + obj.fileName + ")"
+		}
+		
+		return errMessage;
+	},
+	_isErrorObject: function(obj){
+		if(typeof obj === "object"){
+			return ((obj.hasOwnProperty("response") && obj.hasOwnProperty("fileName") && obj.hasOwnProperty("lineNumber")) || (obj.hasOwnProperty("message") && obj.hasOwnProperty("code")));
+		}else{
+			return false;
 		}
 	},
 	info: function(){
