afb7375c3c33dd3f6993f4392db0b5424e4b2ca7
me <me@simpo.org>
2013-01-14 14:04:34 +0000

    Added a global interval function to control interval commands inside an app.

--- layout/pagedColumnList.js
+++ layout/pagedColumnList.js
@@ -52,46 +52,64 @@ define([
 		
 		"_items": [],
 		
-		"itemsPerPage": 5,
+		"itemsPerPage": 100,
 		"page": 1,
 		"_page": 1,
 		"paginationNode": null,
+		"gap": 5,
+		"class": "",
 		
 		postCreate: function(){
 			this._init();
 		},
 		
 		_init: function(){
-			this._parentNode = this.domNode.parentNode;
-			this._initProperties();
-			this._initDom();
-			this._intervalDefaultFunction = this._domCheck;
-			this._setupInterval();
+			try{
+				this._parentNode = this.domNode.parentNode;
+				this._initProperties();
+				this._initDom();
+				this._intervalDefaultFunction = this._domCheck;
+				this._setupInterval();
+			}catch(e){
+				console.info("Could not initiate the widget.");
+			}
 		},
 		
 		_initProperties: function(){
 			// summary:
 			//		Call all the widget property initialization methods.
 			
-			this._initClass();
-			this._initColumnTagName();
+			try{
+				this._initClass();
+				this._initColumnTagName();
+			}catch(e){
+				console.info("Could not initiate the widget properties.");
+			}
 		},
 		
 		_initDom: function(){
 			// summary:
 			//		Call all the widget DOM initialization methods.
 			
-			this._hideNode(this.domNode);
-			this._initWidgetNode();
+			try{
+				this._hideNode(this.domNode);
+				this._initWidgetNode();
+			}catch(e){
+				console.info("Could not initiate the widget Dom.");
+			}
 		},
 		
 		_initClass: function(){
 			// summary:
 			//		Set the class to apply to each column tag.
 			
-			var cClass = domAttr.get(this.domNode, "class");
-			if((cClass !== null) && (cClass !== "")){
-				this["class"] = this._appandItem(this["class"], cClass);
+			try{
+				var cClass = domAttr.get(this.domNode, "class");
+				if(cClass !== null){
+					this["class"] = this._appandItem(this["class"], cClass);
+				}
+			}catch(e){
+				console.info("Could not initiate class name for columns.");
 			}
 		},
 		
@@ -99,8 +117,12 @@ define([
 			// summary:
 			//		Set the type to use for columns.
 			
-			if(this.columnTagName === null){
-				this.columnTagName = this.domNode.tagName.toLowerCase();
+			try{
+				if(this.columnTagName === null){
+					this.columnTagName = this.domNode.tagName.toLowerCase();
+				}
+			}catch(e){
+				console.info("Could not initiate column tag name.");
 			}
 		},
 		
@@ -114,14 +136,18 @@ define([
 			//		lists can be added to, and removed from, via standard Dom
 			//		manipulation methods.
 			
-			if(!this._isElement(this.widgetNode)){
-				this.widgetNode = domConstr.create(
-					"div", {}, this.domNode, "after"
-				);
+			try{
+				if(!this._isElement(this.widgetNode)){
+					this.widgetNode = domConstr.create(
+						"div", {}, this.domNode, "after"
+					);
+				}
+				this._initHiddenNode();
+				this._initColumnListWidget();
+				this._initPaginationNode();
+			}catch(e){
+				console.info("Could not initiate the widget node.");
 			}
-			this._initHiddenNode();
-			this._initColumnListWidget();
-			this._initPaginationNode();
 		},
 		
 		_initPaginationNode: function(){
@@ -130,31 +156,46 @@ define([
 			// returns: object XMLNode
 			//		The new container element.
 			
-			if(!this._isElement(this.paginationNode)){
-				this.paginationNode = domConstr.create(
-					"div", {}, this.widgetNode
-				);
-			}
+			try{
+				if(!this._isElement(this.paginationNode)){
+					this.paginationNode = domConstr.create(
+						"div", {}, this.widgetNode
+					);
+				}
 			
-			return this.containerNode;
+				return this.containerNode;
+			}catch(e){
+				console.info("Could not initiate the pagination node.");
+				return null;
+			}
 		},
 		
 		_initColumnListWidget: function(){
-			if(!this._isWidget(this.columnListWidget)){
-				this.columnListWidget = new columnList({
-					"cols": this.cols,
-					"columnTagName": this.columnTagName,
-					"itemTagName": this.itemTagName
-				});
-				domConstr.place(this.columnListWidget.domNode, this.widgetNode);
+			try{
+				if(!this._isWidget(this.columnListWidget)){
+					this.columnListWidget = new columnList({
+						"cols": this.cols,
+						"columnTagName": this.columnTagName,
+						"itemTagName": this.itemTagName,
+						"gap": this.gap,
+						"class": this.class
+					});
+					domConstr.place(this.columnListWidget.domNode, this.widgetNode);
+				}
+			}catch(e){
+				console.info("Could not initiate the columnList widget.");
 			}
 		},
 		
 		_domCheck: function(){
 			this._clearInterval();
-			this._checkParentNode();
-			this.columnListWidget.cols = this.cols;
-			this._checkNewItems();
+			try{
+				this._checkParentNode();
+				this.columnListWidget.cols = this.cols;
+				this._checkNewItems();
+			}catch(e){
+				console.info("Could not perform the dom checks.");
+			}
 			this._setupInterval();
 		},
 		
@@ -167,123 +208,180 @@ define([
 			// todo:
 			//		Will not work if the nodes position in parent node is moved.
 			
-			if(this.domNode.parentNode !== this._parentNode){
-				this._parentNode = this.domNode.parentNode;
-				domConstr.place(this.widgetNode, this.domNode, "after");
+			try{
+				if(this.domNode.parentNode !== this._parentNode){
+					this._parentNode = this.domNode.parentNode;
+					domConstr.place(this.widgetNode, this.domNode, "after");
+				}
+			}catch(e){
+				console.info("Could not perform parent node check.");
 			}
 		},
 		
 		_checkNewItems: function(){
-			var count = this._items.length;
-			var items = this._getNewItems();
-			if(items.length > 0){
-				this._items = this._items.concat(items);
-				this._removeDeletedItems();
-			}else{
-				this._removeDeletedItems();
-			}
+			try{
+				var count = this._items.length;
+				var items = this._getNewItems();
+				if(items.length > 0){
+					this._items = this._items.concat(items);
+					this._removeDeletedItems();
+				}else{
+					this._removeDeletedItems();
+				}
 			
-			if(count !== this._items.length){
-				this._redraw();
-			}else{
-				this._checkPageChange();
+				if(count !== this._items.length){
+					this._redraw();
+				}else{
+					this._checkPageChange();
+				}
+			}catch(e){
+				console.info("Could not perform check for new items.");
 			}
 		},
 		
 		_checkPageChange: function(){
-			if(this._page !== this.page){
-				this._redraw();
+			try{
+				if(this._page !== this.page){
+					this._redraw();
+				}
+			}catch(e){
+				console.info("Could not perform check for page change.");
 			}
 		},
 		
 		_redraw: function(){
-			this._page = this.page;
-			var items = this._getCurrentPage();
-			this.columnListWidget.clear();
-			array.forEach(items, function(item){
-				domConstr.place(item, this.columnListWidget.domNode);
-			}, this);
-			this._redrawPaginationNode();
+			try{
+				this._page = this.page;
+				var items = this._getCurrentPage();
+				this.columnListWidget.clear();
+				array.forEach(items, function(item){
+					domConstr.place(item, this.columnListWidget.domNode);
+				}, this);
+				this._redrawPaginationNode();
+			}catch(e){
+				console.info("Could not perform page redraw.");
+			}
 		},
 		
 		_redrawPaginationNode: function(){
-			domConstr.empty(this.paginationNode);
-			var pageCount = this._getPageCount();
-			if(this.page > 1){
-				this._addPaginationPrevious();
-			}
-			if(pageCount > 1){
+			try{
+				domConstr.empty(this.paginationNode);
+				var pageCount = this._getPageCount();
+				if(this.page > 1){
+					this._addPaginationPrevious();
+				}
+				if(pageCount > 1){
 				this._addPaginationNumbers();
-			}
-			if(this.page < pageCount){
-				this._addPaginationNext();
+				}
+				if(this.page < pageCount){
+					this._addPaginationNext();
+				}
+			}catch(e){
+				console.info("Could not perform pagination node redraw.");
 			}
 		},
 		
 		_addPaginationNext: function(pageCount){
-			var button = domConstr.create("span", {
-				"innerHTML": "Next"
-			}, this.paginationNode);
+			try{
+				var button = domConstr.create("span", {
+					"innerHTML": "Next"
+				}, this.paginationNode);
 			
-			on(button, "click", lang.hitch(this, function(){
-				var newPageNo = (this.page + 1);
-				this.page = ((newPageNo > pageCount) ? pageCount : newPageNo);
-			}));
+				on(button, "click", lang.hitch(this, function(){
+					var newPageNo = (this.page + 1);
+					this.page = ((newPageNo > pageCount) ? pageCount : newPageNo);
+				}));
+			}catch(e){
+				console.info("Could add next button for pagination.");
+			}
 		},
 		
 		_addPaginationPrevious: function(){
-			var button = domConstr.create("span", {
+			try{
+				var button = domConstr.create("span", {
 				"innerHTML": "Previous"
-			}, this.paginationNode);
+				}, this.paginationNode);
 			
-			on(button, "click", lang.hitch(this, function(){
-				var newPageNo = (this.page - 1);
-				this.page = ((newPageNo > 0) ? newPageNo : 1);
-			}));
+				on(button, "click", lang.hitch(this, function(){
+					var newPageNo = (this.page - 1);
+					this.page = ((newPageNo > 0) ? newPageNo : 1);
+				}));
+			}catch(e){
+				console.info("Could add previous button for pagination.");
+			}
 		},
 		
 		_addPaginationNumbers: function(){
-			var pageCount = this._getPageCount();
+			try{
+				var pageCount = this._getPageCount();
 			
-			var list = domConstr.create("ul", {
-				"style" : {
-						"display": "inline-block",
-				}
-			}, this.paginationNode);
-			for(var pageNo = 1; pageNo <= pageCount; pageNo++){
-				var item = domConstr.create("li", {
+				var list = domConstr.create("ul", {
 					"style" : {
-						"display": "inline-block",
-						"margin": "3px"
-					},
-					"innerHTML": pageNo.toString()
-				}, list);
+							"display": "inline-block",
+					}
+				}, this.paginationNode);
+				for(var pageNo = 1; pageNo <= pageCount; pageNo++){
+					var item = domConstr.create("li", {
+						"style" : {
+							"display": "inline-block",
+							"margin": "3px"
+						},
+						"innerHTML": pageNo.toString()
+					}, list);
 				
-				on(item, "click", lang.hitch(this, function(evt){
-					this.page = parseInt(
-						domAttr.get(evt.target, "innerHTML"), 10
-					);
-				}));
+					on(item, "click", lang.hitch(this, function(evt){
+						this.page = parseInt(
+							domAttr.get(evt.target, "innerHTML"), 10
+						);
+					}));
+				}
+			}catch(e){
+				console.info("Could add page buttons for pagination.");
 			}
 		},
 		
 		_getPageCount: function(){
-			var pageCount = parseInt((this._items.length/this.itemsPerPage), 10);
-			return (((this._items.length % this.itemsPerPage) !== 0) ?
-				pageCount+1 : pageCount
-			);
+			try{
+				var pageCount = parseInt((this._items.length/this.itemsPerPage), 10);
+				return (((this._items.length % this.itemsPerPage) !== 0) ?
+					pageCount+1 : pageCount
+				);
+			}catch(e){
+				console.info("Could not get the page count.");
+				return 1;
+			}
 		},
 		
 		_getCurrentPage: function(){
-			var start = ((this.page-1) * this.itemsPerPage);
-			var end = ((this.page * this.itemsPerPage) - 1);
 			var items = new Array();
 			
-			for(var i = start; ((i <= end) && (i < this._items.length)); i++){
-				items.push(this._items[i]);
+			try{
+				var start = ((this.page-1) * this.itemsPerPage);
+				var end = ((this.page * this.itemsPerPage) - 1);
+				
+			
+				for(var i = start; ((i <= end) && (i < this._items.length)); i++){
+					items.push(this._items[i]);
+				}
+			
+				
+			}catch(e){
+				console.info("Could not get the items for the current page");
 			}
 			
 			return items;
+		},
+		
+		clear: function(){
+			try{
+				this.columnListWidget.clear();
+				array.forEach(this._items, function(item){
+					domConstr.destroy(item);
+				}, this);
+				this._items = new Array();
+			}catch(e){
+				console.info("Could not clear the widget of items.");
+			}
 		},
 		
 		_getNewItems: function(parentNode){
@@ -294,28 +392,38 @@ define([
 			// returns: array() XMLNode()
 			
 			parentNode = ((parentNode === undefined) ? this.domNode : parentNode);
-			
 			var newItems = new Array();
-			var items = $(this.itemTagName, parentNode);
-			array.forEach(items, function(item){
-				if(item.parentNode === parentNode){
-					newItems.push(item);
-				}
-			}, this);
-			items.remove();
+			
+			try{
+				var items = $(this.itemTagName, parentNode);
+				array.forEach(items, function(item){
+					if(item.parentNode === parentNode){
+						newItems.push(item);
+					}
+				}, this);
+				items.remove();
+			}catch(e){
+				console.info("Could not get new items");
+				return new Array();
+			}
+			
 			return newItems;
 		},
 		
 		_removeDeletedItems: function(){
-			var editedList = new Array();
+			try{
+				var editedList = new Array();
 			
-			array.forEach(this._items, function(item){
-				if(this._isElement(item)){
-					editedList.push(item);
-				}
-			}, this);
+				array.forEach(this._items, function(item){
+					if(this._isElement(item)){
+						editedList.push(item);
+					}
+				}, this);
 			
-			this._items = editedList;
+				this._items = editedList;
+			}catch(e){
+				console.info("Could not remove deleted items from the item list");
+			}
 		}
 	});
 	
